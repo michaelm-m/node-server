844107b1eefc40402466d755074333e26e6c88ef
Michael <mcloughlin34@hotmail.com>
2019-05-28 23:39:40 -0400

    use env vars

--- index.js
+++ index.js
@@ -10,26 +10,13 @@ app.get('/', function (req, res) {
 });
 
 app.get('/hello', function (req, res) {
- res.send('*hello-env-dev-2 : ' + process.env.CUSTOM_ENV2 + ' ' + process.env.CUSTOM_ENV + ' ::: ' + process.env.CONSOLE_PROP);
+  const message = 'Hello ' + process.env.CUSTOM_ENV;
+ res.send(message);
 });
 
 
 //listen to port 3000 by default
 app.listen(3000);
 console.log('Running on Port : 3000');
-
-
-// var redis = require("redis"),
-//     client = redis.createClient();
-//
-// client.on("connect", function () {
-//   console.log('REDIS connect');
-//     client.set("bar", JSON.stringify(myJson));
-//     client.get("bar", function (err, value) {
-//       if (err) throw(err);
-//       var testJson = JSON.parse(value);
-//       console.log(testJson.glossary.title);
-//     });
-// });
 
 module.exports = app;
