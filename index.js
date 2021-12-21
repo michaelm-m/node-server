97b43625fc2700569af4d38598591cb099fe6d03
michaelm-m <mcloughlin34@hotmail.com>
2021-12-21 00:29:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,27 +4,20 @@ const app = express();
 const port = 8081;
 
 app.get('/', (req, res) => {
-  const responseText = '*Node Application ::: Hello : ' + process.env.CUSTOM_ENV + ' has ' + process.env.CUSTOM_ENV_2;
+  const responseText = 'Hello from the api server';
+  res.send(responseText);
+});
+
+app.get('/products', (req, res) => {
+  const responseText = `I will return all products`;
+  res.send(responseText);
+});
+
+app.get('/products/:productId', (req, res) => {
+  const responseText = `I will return product: ${req.params.productId}`;
   res.send(responseText);
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 });
-
-//
-// var http = require("http");
-//
-// http.createServer(function (request, response) {
-//    // Send the HTTP header
-//    // HTTP Status: 200 : OK
-//    // Content Type: text/plain
-//    response.writeHead(200, {'Content-Type': 'text/plain'});
-//    var test = process.env.CUSTOM_ENV;
-//    var responseText = 'PASSWORD TEST : ' + test;
-//    // Send the response body as "Hello World"
-//    response.end(responseText);
-// }).listen(8081);
-//
-// // Console will print the message
-// console.log('Server running at http://127.0.0.1:8081/');
